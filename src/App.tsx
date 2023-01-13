661da40b6eda4644c8e6a781bc4ffb36f50991b5
simple1017 <simple01281017@gmail.com>
2023-01-13 22:37:06 +0800

    feat: add interactive pricing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import PricingMain from "./components/PriceMain";
+import { PriceTable } from "./type";
+
+const initialPriceTable: PriceTable[] = [
+  { viewRange: "10K", price: 8.0 },
+  { viewRange: "50K", price: 12.0 },
+  { viewRange: "100K", price: 16.0 },
+  { viewRange: "500K", price: 24.0 },
+  { viewRange: "1M", price: 36.0 },
+];
 
 function App() {
+  const [priceTable, setPriceTable] = useState<PriceTable[]>(initialPriceTable);
+  const [sliderValue, setSliderValue] = useState<number>(0);
+  const [viewRange, setViewRange] = useState<string>("10K");
+  const [price, setPrice] = useState<number>(priceTable[0].price);
+  const [switchChecked, setSwitchChecked] = useState<boolean>(false);
+  const [trial, setTrial] = useState<boolean>(false);
+
+  const handleSliderChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const index = Number(event.target.value);
+    setSliderValue(index);
+    setViewRange(priceTable[index].viewRange);
+    setPrice(priceTable[index].price);
+  };
+
+  const handleSwitchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.checked) {
+      const discountPrice = initialPriceTable.map((priceRow) => {
+        return { viewRange: priceRow.viewRange, price: priceRow.price * 0.75 };
+      });
+      setPriceTable(discountPrice);
+      setPrice(discountPrice[sliderValue].price);
+    } else {
+      setPriceTable(initialPriceTable);
+      setPrice(initialPriceTable[sliderValue].price);
+    }
+    setSwitchChecked(event.target.checked);
+  };
+
+  const handleTrialButton = () => {
+    setTrial(true);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App-title">Simple, traffic-based pricing</h1>
+      <div className="App-sub-title">
+        <span>Sign-up for our 30-day trial.</span>
+        <span>No credit card required.</span>
+      </div>
+
+      <PricingMain
+        onSliderChange={handleSliderChange}
+        sliderValue={sliderValue}
+        switchChecked={switchChecked}
+        onSwitchChange={handleSwitchChange}
+        onClickTrail={handleTrialButton}
+        viewRange={viewRange}
+        price={price}
+        trail={trial}
+      />
     </div>
   );
 }
